b8b9fe283ab86764733b857ba6cafc3eb75d98ea
andi545 <ff2505@haw-ingolstadt.de>
2013-09-15 08:33:08 +0200

    Webserver.java

--- Webserver.java
+++ Webserver.java
@@ -4,10 +4,19 @@ import java.net.*;
 class Webserver {
 	ServerSocket providerSocket;
 	Socket connection = null;
+	URL requestedUrl = null;
+	HttpURLConnection connection = null;
+	int PORT = 2013;
+	int BACKLOG = 10; // maximum queue length for incoming conn. requests
+
 
 	void run() {
-		providerSocket = new ServerSocket(2004, 10);
-		Syste.out.println("Waiting for connection");
+			try {
+		providerSocket = new ServerSocket(PORT, BACKLOG);
+		System.out.println("Waiting for connection");
 		connection = providerSocket.accept();
+			} catch(IOException e) {
+					System.err.println("Could not create server socket");
+			}
 	}
 }
